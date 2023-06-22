ecd50e2ed5ed9df059c99024553ca72e2dc126e7
Lucas Gonzaga <lucassoft@gmx.com>
2023-06-22 19:35:44 -0300

    Create index2.js

---
+++ aula9/index2.js
@@ -0,0 +1,8 @@
+const nome = 'Lucas' //STRING
+const nome1 = 'Lucas' //STRING
+const nome2 = 'Lucas' //STRING
+const num1 = 10 //NUMBER
+const num2 = 10.52 //NUMBER
+let nomeAluno; //UNDEFINED = NÃO APONTA PRA LUGAR ALGUM NA MEMORIA
+let sobrenomeAluno = null // NULO = NÃO APONTA PRA LUGAR ALGUM NA MEMORIA
+const aprovado = true //Boolean = true e false
