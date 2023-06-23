3cb33cfbf9d070ad91b6a637b644fc9f13011272
Lucas Gonzaga <lucassoft@gmx.com>
2023-06-23 08:56:47 -0300

    aula 10

---
+++ aula10/index.js
@@ -0,0 +1,16 @@
+/*
+    Aritiméticos:
+(+) Adição e Concatenação
+(-) Subtração , (/) Divisão , (*) Multiplicação
+(**) Potenciação
+(%) Resto de divisão
+
+    Precedência:
+() > ** > * , / , % > + , - 
+
+*/
+
+const num1 = 5
+const num2 = 10
+const num3 = 15
+console.log(num1 + num2 + num1 * num3)
