17b8d423c7d418953aa4f197356a723fecccd106
Lucas Gonzaga <lucassoft@gmx.com>
2023-06-24 10:41:01 -0300

    aula 13 e 14

---
+++ aula13/index.js
@@ -0,0 +1,8 @@
+//---------------01234567
+let umaString = 'Um texto'
+
+console.log(umaString.concat(' mais um texto'))
+console.log(umaString + ' mais um texto')
+console.log(`${umaString} mais um texto`)// MELHOR OPCAO
+
+console.log(umaString.replace('Um', 'Dois'))
