347348a680223070ef0f022325466d560d09f533
Lucas Gonzaga <lucassoft@gmx.com>
2023-07-02 14:02:58 -0300

    Update index.js

--- aula14/index.js
+++ aula14/index.js
@@ -1,4 +1,7 @@
-let num1 = 1
-let num2 = 2.5
+let num1 = 10 //number 
+let num2 = 2.5 //number
 
-console.log(num1.toString() + num2)
+//console.log(num1.toString() + num2)
+//console.log(typeof num1)
+//num1 = num1.toString()
+//console.log(num1.toFixed(2))
