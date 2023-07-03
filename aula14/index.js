3345deaf55a5c7140e0883d105f7d84890c8c74f
Lucas Gonzaga <lucassoft@gmx.com>
2023-07-03 18:59:20 -0300

    aula14

--- aula14/index.js
+++ aula14/index.js
@@ -1,7 +1,20 @@
-let num1 = 10 //number 
-let num2 = 2.5 //number
+let num1 = 0.7 //number 
+let num2 = 0.1 //number
+
+num1 = ((num1*100) + (num2*100)) / 100
+num1 = ((num1*100) + (num2*100)) / 100
+num1 = ((num1*100) + (num2*100)) / 100
+
+//num1 = num1.toFixed(num1)
+
+console.log(num1)
+console.log(Number.isInteger(num1))
 
 //console.log(num1.toString() + num2)
 //console.log(typeof num1)
 //num1 = num1.toString()
 //console.log(num1.toFixed(2))
+//console.log(Number.isInteger(num1))
+//console.log(Number.isNaN(temp))
+//let temp = num1 * '5'
+//console.log(temp)
