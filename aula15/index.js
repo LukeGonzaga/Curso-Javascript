72b0c7e5e26a59b73d05df923d386cf92df95fe4
Lucas Gonzaga <lucassoft@gmx.com>
2023-07-03 19:19:25 -0300

    aula15

---
+++ aula15/index.js
@@ -0,0 +1,17 @@
+console.log(100 / 0.000000000001)
+
+//let num1 = 9
+
+
+//console.log(num1 ** (1/2))
+//console.log(num1 **0.5)
+
+//let num2 = Math.floor(num1) 
+//let num2 = Math.ceil(num1)
+//let num2 = Math.round(num1)
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,98))
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,98
+//const aleatorio = Math.random() * (10-5)+5
+
+//console.log(Math.PI);
+//console.log(Math.pow(2,10))
